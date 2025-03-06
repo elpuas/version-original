ac20f36c0c0ceeee4e10e4edcbb06c9316434c4f
Alfredo Navas <elpuas@gmail.com>
2025-03-05 21:11:46 -0600

    On branch feature/ACF-files
     Changes to be committed:
    	modified:   acf-json/group_67c90167cf4d9.json
    	modified:   patterns/header.php
    	modified:   templates/index.html

--- patterns/header.php
+++ patterns/header.php
@@ -8,42 +8,25 @@
 ?>
 
 <!-- wp:group {"layout":{"inherit":"true","type":"constrained"}} -->
-<div class="wp-block-group">
-	<!-- wp:cover {"url":"<?php echo esc_url( get_theme_file_uri( 'src/images/hero-background.jpg' ) ) ?>","id":26,"dimRatio":50,"isUserOverlayColor":true,"contentPosition":"center center","align":"full","layout":{"type":"constrained"}} -->
-	<div class="wp-block-cover alignfull"><span aria-hidden="true"
-			class="wp-block-cover__background has-background-dim"></span><img
-			class="wp-block-cover__image-background wp-image-26" alt=""
-			src="<?php echo esc_url( get_theme_file_uri( 'src/images/header-pattern.jpg' ) ) ?>"
-			data-object-fit="cover" />
-		<div class="wp-block-cover__inner-container">
-			<!-- wp:group {"layout":{"type":"constrained","contentSize":"50%"}} -->
-			<div class="wp-block-group">
-				<!-- wp:group {"layout":{"type":"flex","flexWrap":"nowrap","justifyContent":"center"}} -->
-				<div class="wp-block-group">
-					<!-- wp:separator {"style":{"layout":{"selfStretch":"fill","flexSize":null}},"backgroundColor":"white"} -->
-					<hr
-						class="wp-block-separator has-text-color has-white-color has-alpha-channel-opacity has-white-background-color has-background" />
-					<!-- /wp:separator -->
+<div class="wp-block-group"><!-- wp:cover {"url":"<?php echo esc_url( get_theme_file_uri( 'src/images/header-pattern.jpg' ) ) ?>","id":26,"dimRatio":50,"isUserOverlayColor":true,"contentPosition":"center center","align":"full","layout":{"type":"constrained"}} -->
+<div class="wp-block-cover alignfull"><span aria-hidden="true" class="wp-block-cover__background has-background-dim"></span><img class="wp-block-cover__image-background wp-image-26" alt="" src="<?php echo esc_url( get_theme_file_uri( 'src/images/header-pattern.jpg' ) ) ?>" data-object-fit="cover"/><div class="wp-block-cover__inner-container"><!-- wp:group {"layout":{"type":"constrained","contentSize":"50%"}} -->
+<div class="wp-block-group"><!-- wp:group {"layout":{"type":"flex","flexWrap":"nowrap","justifyContent":"center"}} -->
+<div class="wp-block-group"><!-- wp:separator {"style":{"layout":{"selfStretch":"fill","flexSize":null}},"backgroundColor":"white"} -->
+<hr class="wp-block-separator has-text-color has-white-color has-alpha-channel-opacity has-white-background-color has-background"/>
+<!-- /wp:separator -->
 
-					<!-- wp:paragraph -->
-					<p>VERSION ORIGINAL</p>
-					<!-- /wp:paragraph -->
+<!-- wp:paragraph -->
+<p>VERSION ORIGINAL</p>
+<!-- /wp:paragraph -->
 
-					<!-- wp:separator {"style":{"layout":{"selfStretch":"fill","flexSize":null}},"backgroundColor":"white"} -->
-					<hr
-						class="wp-block-separator has-text-color has-white-color has-alpha-channel-opacity has-white-background-color has-background" />
-					<!-- /wp:separator -->
-				</div>
-				<!-- /wp:group -->
+<!-- wp:separator {"style":{"layout":{"selfStretch":"fill","flexSize":null}},"backgroundColor":"white"} -->
+<hr class="wp-block-separator has-text-color has-white-color has-alpha-channel-opacity has-white-background-color has-background"/>
+<!-- /wp:separator --></div>
+<!-- /wp:group -->
 
-				<!-- wp:heading {"textAlign":"center","style":{"spacing":{"margin":{"top":"0","bottom":"0"}}},"fontSize":"x-large","fontFamily":"cormorant-garamond"} -->
-				<h2 class="wp-block-heading has-text-align-center has-cormorant-garamond-font-family has-x-large-font-size"
-					style="margin-top:0;margin-bottom:0">NUESTRO MENU</h2>
-				<!-- /wp:heading -->
-			</div>
-			<!-- /wp:group -->
-		</div>
-	</div>
-	<!-- /wp:cover -->
-</div>
+<!-- wp:heading {"textAlign":"center","style":{"spacing":{"margin":{"top":"0","bottom":"0"}}},"fontSize":"x-large","fontFamily":"cormorant-garamond"} -->
+<h2 class="wp-block-heading has-text-align-center has-cormorant-garamond-font-family has-x-large-font-size" style="margin-top:0;margin-bottom:0">NUESTRO MENU</h2>
+<!-- /wp:heading --></div>
+<!-- /wp:group --></div></div>
+<!-- /wp:cover --></div>
 <!-- /wp:group -->
